69311c475a403acb05eea3739611a06ff874e97b
oussama <oussama.elmtili@sia-partners.com>
2023-04-21 10:18:48 +0200

    Notebook first steps

--- initialization/init.py
+++ initialization/init.py
@@ -1,8 +1,7 @@
 import os
-import pathlib
-
-
 def init_project():
+    print(os.getcwd())
+    os.system('cmd /c "cd .."')
     os.system('cmd /c "pip install pipenv"')
     os.system('cmd /c "pipenv install -r requirements.txt"')
 
